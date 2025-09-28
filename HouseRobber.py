9741662881128f4574de2e80e6974bc3471a384f
JavierKrick <javierkrick@gmail.com>
2025-09-28 03:57:11 -0300

    Add files via upload

--- HouseRobber.py
+++ HouseRobber.py
@@ -1,3 +1,25 @@
+##Soluciones Bottom-Up
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 0: return 0
+        if n == 1: return nums[0]
+        if n == 2: return max(nums[0],nums[1])
+        primero = nums[0]
+        ultimo = max(nums[0],nums[1])
+
+        for i in range(2,n):
+            aux = primero
+            primero = ultimo
+            ultimo = max(aux+nums[i],ultimo)
+
+        return ultimo
+
+
+
+"""   SE PODÍA REDUCIR EL ESPACIO
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
@@ -14,6 +36,4 @@ class Solution:
 
         return dp[-1]
 
-
-
-        
+"""
